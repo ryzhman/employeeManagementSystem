757f1fb0bde121adb04b82fec4e7994c8a274289
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-05-11 23:05:39 -0400

    Adding mapping for project files using MVC

---
+++ src/main/java/com/go2it/edu/emplyeenanagementsystem/config/MvcConfig.java
@@ -0,0 +1,35 @@
+package com.go2it.edu.emplyeenanagementsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Alex Ryzhkov
+ */
+@EnableWebMvc
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+	/**
+	 * This method is used to map the resource being accessed from the client to the 'internal' version
+	 *
+	 * @param registry
+	 */
+	@Override
+	public void addResourceHandlers(
+			ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("/static/**")
+				.addResourceLocations("/WEB-INF/view/react-app/build/static/");
+		registry.addResourceHandler("/*.js")
+				.addResourceLocations("/WEB-INF/view/react-app/build/");
+		registry.addResourceHandler("/*.json")
+				.addResourceLocations("/WEB-INF/view/react-app/build/");
+		registry.addResourceHandler("/*.ico")
+				.addResourceLocations("/WEB-INF/view/react-app/build/");
+		registry.addResourceHandler("/index.html")
+				.addResourceLocations("/WEB-INF/view/react-app/build/index.html");
+	}
+}

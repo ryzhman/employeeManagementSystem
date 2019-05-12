7c6713d3532c1c24268934049907e5b007f692af
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-05-11 23:02:08 -0400

    Creating the basic Spring Boot application

---
+++ src/main/java/com/go2it/edu/emplyeenanagementsystem/EmplyeeNanagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.go2it.edu.emplyeenanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplyeeNanagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplyeeNanagementSystemApplication.class, args);
+	}
+
+}

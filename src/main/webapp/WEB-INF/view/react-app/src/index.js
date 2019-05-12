5d460bfd7ac87828d7ba346e29fb5d48210c5699
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-05-11 23:47:19 -0400

    Adding usage of React components

--- src/main/webapp/WEB-INF/view/react-app/src/index.js
+++ src/main/webapp/WEB-INF/view/react-app/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Form from './component/Form'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const inputs = [{
+    name: "username",
+    placeholder: "username",
+    type: "text"
+}, {
+    name: "password",
+    placeholder: "password",
+    type: "password"
+}, {
+    type: "submit",
+    value: "Submit",
+    className: "btn"
+}];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const props = {
+    name: 'loginForm',
+    method: 'POST',
+    action: '/perform_login',
+    inputs: inputs
+};
+
+const params = new URLSearchParams(window.location.search);
+
+ReactDOM.render(
+    //If credentials are not valid, Spring Security redirects us to
+    //index.html?error=true and we handle this request param in Form component
+    <Form {...props} error={params.get('error')}/>,
+    document.getElementById('container'));
